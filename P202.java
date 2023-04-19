f3cfe6237d497d90b84bcf3679ec5bad79e939dc
tirthb101 <bhatiatirth01@gmail.com>
2023-04-20 00:10:10 +0530

    practical 1-3

--- P202.java
+++ P202.java
@@ -1,10 +1,22 @@
+
 /*
 Write a program that prompts the user to enter a letter and
 check whether a letter is a vowel or constant.
  */
+import java.util.Scanner;
 
 public class P202 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a charactor: ");
+        char data = sc.next().charAt(0);
 
+        if (data == 'a' || data == 'A' || data == 'e' || data == 'E' || data == 'i' || data == 'I' || data == 'o'
+                || data == 'O' || data == 'u' || data == 'U') {
+            System.out.println("VOWEL");
+        } else {
+            System.out.println("CONSONANT");
+        }
+        sc.close();
     }
 }
